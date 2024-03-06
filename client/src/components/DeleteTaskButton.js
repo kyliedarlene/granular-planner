44c6c76de77d6e757bff6ffc2648bab84bf4a0fa
Kylie Clark <kyliedarlene@icloud.com>
2024-03-06 14:52:52 -0700

    handleDeleteRoutineTask updating DOM

--- client/src/components/DeleteTaskButton.js
+++ client/src/components/DeleteTaskButton.js
@@ -1,16 +1,5 @@
 function DeleteTaskButton({ routineTask, handleDeleteRoutineTask }) {
-
-    // function deleteRoutineTask(routineTask) {
-    //     //console.log(routineTask)
-    //     //console.log(routineTask[0].id)
-    //     fetch (`/routine_tasks/${routineTask.id}`, {
-    //         method: "DELETE",
-    //     })
-    // }
-    
-    return (
-        <button onClick={() => handleDeleteRoutineTask(routineTask)}>[x]</button>
-    )
+    return <button onClick={() => handleDeleteRoutineTask(routineTask)}>[x]</button>
 }
 
 export default DeleteTaskButton;
