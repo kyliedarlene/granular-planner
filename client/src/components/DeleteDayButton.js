a6a558fc87a492e2599756e736fe049ca83275ec
MichaelPile <mjpile@gmail.com>
2024-03-07 12:23:54 -0800

    adds DeleteDayButton

---
+++ client/src/components/DeleteDayButton.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function DeleteDayButton({day}) {
+
+    const navigate = useNavigate();
+
+    function handleDeleteDay(day){
+        console.log(day)
+        const id = day.id
+        //console.log(id)
+        fetch (`/days/${id}`, {
+            method: "DELETE",
+        })
+        .then(() => {
+            // Navigate back to the previous page
+            navigate("/days")
+        })
+        //nav to days
+    }
+
+    return <button onClick={() => handleDeleteDay(day)}>[delete {day.name}]</button>
+}
+
+export default DeleteDayButton;
