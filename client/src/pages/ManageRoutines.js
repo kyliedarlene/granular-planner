7f917f4d4274a713ebcfa325db5029494fc80201
MichaelPile <mjpile@gmail.com>
2024-03-07 10:46:54 -0800

    adds NavBar

--- client/src/pages/ManageRoutines.js
+++ client/src/pages/ManageRoutines.js
@@ -1,8 +1,10 @@
 import RoutineMenu from "../components/RoutineMenu";
+import NavBar from "../components/NavBar";
 
 function ManageRoutines() {
     return (
         <>
+            <NavBar/>
             <h1>Select a routine to update:</h1>
             <RoutineMenu />
         </>
